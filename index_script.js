5d361fb1ab8c7b4dc7879562f6ee951b54fee1b4
Kenneth Inboden <kinboden@lakelandcollege.edu>
2021-03-09 16:56:09 +0000

    test hyperlink tied to function call button

--- index_script.js
+++ index_script.js
@@ -1,4 +1,8 @@
 function getUsername() {
   var player = document.getElementById("player").value;
   console.log(player);
+  return player;
+}
+function nextPage() {
+  location.href="random_god_generator.html";
 }
