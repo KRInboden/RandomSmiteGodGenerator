23f2ff3f977b1788bf7af7b77943005a46cc8536
Kenneth Inboden <kinboden@lakelandcollege.edu>
2021-03-10 02:21:27 +0000

    store input in local storage for access later

--- index_script.js
+++ index_script.js
@@ -1,5 +1,6 @@
 function getUsername() {
   var player = document.getElementById("player").value;
+  localStorage.setItem("username",player);
   console.log(player);
   window.location.replace("random_god_generator.html");
 }
