586b308bdf7be860fcfa651fa6fa3886cbf5c028
Kenneth Inboden <kinboden@lakelandcollege.edu>
2021-03-16 18:52:09 +0000

    add paladins username for paladins service expansion

--- index_script.js
+++ index_script.js
@@ -1,6 +1,9 @@
-function getUsername() {
-  var player = document.getElementById("player").value;
-  localStorage.setItem("username",player);
-  console.log(player);
-  window.location.replace("random_god_generator.html");
+function getUsernames() {
+  var playerSmite = document.getElementById("smiteUsername").value;
+  var playerPaladins = document.getElementById("paladinsUsername").value;
+  localStorage.setItem("_usernameSmite",playerSmite);
+  localStorage.setItem("_usernamePaladins",playerPaladins);
+  console.log(playerSmite);
+  console.log(playerPaladins);
+  window.location.href = "random_god_generator.html";
 }
