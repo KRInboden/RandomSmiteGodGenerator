042b7d2a59c10f5d44d19ef37f45b636ae495a4d
Kenneth Inboden <kinboden@lakelandcollege.edu>
2021-03-16 19:29:05 +0000

    comment out paladins coding (still under construction)

--- index_script.js
+++ index_script.js
@@ -1,21 +1,14 @@
 function getUsernames() {
   var playerSmite = document.getElementById("smiteUsername").value;
-  var playerPaladins = document.getElementById("paladinsUsername").value;
+  ///Paladins coding ---var playerPaladins = document.getElementById("paladinsUsername").value;
   localStorage.setItem("_usernameSmite",playerSmite);
-  localStorage.setItem("_usernamePaladins",playerPaladins);
+  ///Paladins coding ---localStorage.setItem("_usernamePaladins",playerPaladins);
   console.log(playerSmite);
-  console.log(playerPaladins);
-  ///window.location.href = "random_god_generator.html";
+  ///Paladins coding ---console.log(playerPaladins);
 }
-///function nextPage() {
-  ///evt.preventDefault();
- /// window.location.replace("random_god_generator.html");
-///  return false;
-///}
 
-///btn.addEventListener("click", nextPage());
 document.getElementById("btn").addEventListener("click", function(evt) {
   evt.preventDefault();
-  window.location.replace("random_god_generator.html");
+  window.location.replace("random_god_generator_smite.html");
   return false;
 });
