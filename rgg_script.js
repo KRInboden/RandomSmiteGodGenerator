111b6b7ac53f42e2ac7686e09b1399818718b462
Kenneth Inboden <kinboden@lakelandcollege.edu>
2021-03-16 21:58:47 +0000

    add pictures and name of random god

--- rgg_script.js
+++ rgg_script.js
@@ -1,12 +1,45 @@
-function selectRandomGod() {
-  var rndInt = Math.floor(Math.random()*113);
-  console.log(rndInt)
-}
+let godList = [];
+let picture = document.getElementById("hero");
+var img=new Image();
+    
+// var picture = new Image(200,200); 
 fetch("gods.json")
   .then(function(resp) {
     return resp.json();
   })
-  .then(function(data){
-    console.log(data);
+  .then(function(data) {
+    godList = data
+    
+    let god = godList[selectRandomGod()];
+    console.log(god.name);
+    let name = god.name;
+    let letter = god.name[0].toLowerCase();
+    // let safeName = "";
+
+    // for (const letter of name){
+    //   if(letter!=" ")
+    //     safeName += letter;
+    // }
+
+    name = name.replace(/\s/g, '');
+
+    let path =`Smite_Project_Site/gods/${letter}/${name}.jpg`
+    preloadImage(path, changePicture)
+  // Create new img element
+    img.src=`Smite_Project_Site/gods/${letter}/${name}.jpg`;
+    picture.src = img.src;
   });
-selectRandomGod();
+
+function selectRandomGod() {
+  return parseInt(Math.floor(Math.random()*113));
+  //console.log(rndInt); 
+}
+function preloadImage(url, callback)
+{
+    img.src=url;
+    img.onload = callback;
+}
+function changePicture()
+{
+    picture.src = img.src;
+}
