e803f5ed741999b5f52629a27e78996f01b17b75
Kenneth Inboden <kinboden@lakelandcollege.edu>
2021-03-17 19:17:42 +0000

    add button to randomly select god

--- rgg_script.js
+++ rgg_script.js
@@ -2,36 +2,35 @@ let godList = [];
 let picture = document.getElementById("hero");
 var img=new Image();
     
-fetch("gods.json")
-  .then(function(resp) {
-    return resp.json();
-  })
-  .then(function(data) {
-    godList = data
-    
-    let god = godList[selectRandomGod()];
-    console.log(god.name);
-    let name = god.name;
-    let letter = god.name[0].toLowerCase();
+function randomizer() {
+  fetch("gods.json")
+    .then(function(resp) {
+      return resp.json();
+    })
+    .then(function(data) {
+      godList = data
+      
+      let god = godList[selectRandomGod()];
+      console.log(god.name);
+      let name = god.name;
+      let letter = god.name[0].toLowerCase();
 
-    name = name.replace(/\s/g, '');
+      name = name.replace(/\s/g, '');
 
-    let path =`Smite_Project_Site/gods/${letter}/${name}.jpg`
-    preloadImage(path, changePicture)
-  // Create new img element
-    img.src=`Smite_Project_Site/gods/${letter}/${name}.jpg`;
-    picture.src = img.src;
-  });
-
+      let path =`Smite_Project_Site/gods/${letter}/${name}.jpg`
+      preloadImage(path, changePicture)
+    // Create new img element
+      img.src=`Smite_Project_Site/gods/${letter}/${name}.jpg`;
+      picture.src = img.src;
+    });
+}
 function selectRandomGod() {
   return parseInt(Math.floor(Math.random()*113));
 }
-function preloadImage(url, callback)
-{
+function preloadImage(url, callback) {
     img.src=url;
     img.onload = callback;
 }
-function changePicture()
-{
+function changePicture() {
     picture.src = img.src;
 }
