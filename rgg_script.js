448b7287898db34afbafc67d117c60b45bb24650
Kenneth Inboden <kinboden@lakelandcollege.edu>
2021-03-26 16:08:38 +0000

    add god info to textbox in smite page

--- rgg_script.js
+++ rgg_script.js
@@ -8,10 +8,18 @@ function randomizer() {
       return resp.json();
     })
     .then(function(data) {
-      godList = data
+      godList = data;
+
+      
       
       let god = godList[selectRandomGod()];
-      console.log(god.name);
+      //saving god info into variables
+      var role = god.role;
+      var pantheon = god.pantheon;
+      var attack_type = god.attack_type;
+      var power_type = god.power_type;
+
+      
       let name = god.name;
       let letter = god.name[0].toLowerCase();
       //remove spaces and apostrophes
@@ -24,7 +32,37 @@ function randomizer() {
       img.src=`Smite_Project_Site/gods/${letter}/${name}.jpg`;
       picture.src = img.src;
 
-      document.getElementById("godName").innerHTML = god.name;
+      //removing previous txt nodes
+      document.getElementById("godName").innerHTML = "";
+      document.getElementById("godRole").innerHTML = "";
+      document.getElementById("godAttack").innerHTML = "";
+      document.getElementById("godPower").innerHTML = "";
+      document.getElementById("godPantheon").innerHTML = "";
+
+      //creating txt node on page
+      var _name = document.getElementById("godName");
+      var godName = document.createTextNode("Name: " + god.name);
+      _name.appendChild(godName);
+      
+      var _role = document.getElementById("godRole");
+      var godRole = document.createTextNode("Role: " + role);
+      _role.appendChild(godRole);
+      
+      var _attackType = document.getElementById("godAttack");
+      var godAttackType = document.createTextNode("Attack Type: " + attack_type);
+      _attackType.appendChild(godAttackType);
+      
+      var _powerType = document.getElementById("godPower");
+      var godPowerType = document.createTextNode("Power Type: " + power_type);
+      _powerType.appendChild(godPowerType);
+      
+      var _pantheon = document.getElementById("godPantheon");
+      var godPantheon = document.createTextNode("Pantheon: " + pantheon);
+      _pantheon.appendChild(godPantheon);
+
+      console.clear();
+      console.log(god.name);
+      console.log(role, pantheon, attack_type, power_type);
     });
 }
 function selectRandomGod() {
